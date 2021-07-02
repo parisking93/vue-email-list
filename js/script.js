5e1d8653ce6a3b38c3c2c762f0521c9f21932e63
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-07-02 15:34:27 +0200

    add print arrayEmail aspettando l'api

--- js/script.js
+++ js/script.js
@@ -4,7 +4,18 @@ const app = new Vue(
     {
         el : '#app',
         data : {
-            
+            arrayEmail : []
+        },
+        mounted() {
+            for(let i = 0; i < 10; i++) {
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(element => {
+                    this.arrayEmail.push(element.data.response);
+                    console.log(element.data.response);
+                });   
+            }
+
         }
     }
 )
